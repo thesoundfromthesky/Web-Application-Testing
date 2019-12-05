28f5afff0eaadbcb7c82acf13bafd3a25a882f20
thesoundfromthesky <nagara@naver.com>
2019-12-05 14:12:25 -0600

    display HTML

--- web-application-testing/src/App.js
+++ web-application-testing/src/App.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 import { Dashboard, Display } from "./components";
 function App() {
+  const [record, setRecord] = useState({ strikes: 0, balls: 0 });
   return (
     <div className="App">
+      <Display record={record} />
       <Dashboard />
-      <Display />
     </div>
   );
 }
--- web-application-testing/src/components/Display.js
+++ web-application-testing/src/components/Display.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export function Display() {
-  return <div>Display</div>;
+export function Display(props) {
+  const { balls, strikes } = props.record;
+  return (
+    <div>
+      <h1>Record Board</h1>
+      <div>balls:{balls}</div>
+      <div>strikes:{strikes}</div>
+    </div>
+  );
 }
